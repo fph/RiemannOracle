0ae567dd336afa3b190f4ae327417e3f09380e60
Federico Poloni <federico.poloni@unipi.it>
2024-02-03 15:50:43 +0100

    A Hessian that seems to work

--- nearest_singular.m
+++ nearest_singular.m
@@ -1,12 +1,12 @@
-function problem = nearest_singular(structure, A, epsilon, y)
+function problem = nearest_singular(structure, A, epsilon, y, use_hessian)
 
 if isempty(structure)
     structure = A ~= 0;
 end
-if not(exist('epsilon', 'var'))
+if not(exist('epsilon', 'var')) || isempty(epsilon)
     epsilon = 0.0;
 end
-if not(exist('y', 'var'))
+if not(exist('y', 'var')) || isempty(y)
     y = 0;
 end
 
@@ -15,6 +15,9 @@ problem.M = spherecomplexfactory(n);
 
 problem.cost  = @(v, store) cost(structure, A, v, epsilon, y, store);
 problem.egrad = @(v, store) egrad(structure, A, v, epsilon, y, store);
+if use_hessian
+    problem.ehess = @(v, w, store) ehess(structure, A, v, epsilon, y, w, store);
+end
 problem.minimizer = @(v, store) minimizer(structure, A, v, epsilon, y, store);
 end
 
@@ -38,7 +41,7 @@ function [eg, store] = egrad(structure, A, v, epsilon, y, store)
     store = populate_store(structure, A, v, epsilon, y, store);
     r = store.r;
     d = store.d;
-    z = r .* d;
+    z = d .* r;
     eg = -2*(A' * z + (structure' * (conj(z) .* z) .* v)); 
 end
 
@@ -46,6 +49,18 @@ function E = minimizer(structure, A, v, epsilon, y, store)
     store = populate_store(structure, A, v, epsilon, y, store);
     r = store.r;
     d = store.d;
-    z = r .* d;
+    z = d .* r;
     E = z .* (v' .* structure);
+end
+
+function [eh, store] = ehess(structure, A, v, epsilon, y, w, store)
+    store = populate_store(structure, A, v, epsilon, y, store);
+    r = store.r;
+    d = store.d;
+    z = d .* r;
+    rightpart = -A*w - z .* (structure * (conj(v) .* w + conj(w) .* v));
+    dz = store.d .* rightpart;
+    dEtz = (structure' * (conj(z) .* z)) .* w + (structure' * (conj(dz) .* z)) .* v;
+    Etdz = (structure' * (conj(z) .* dz)) .* v;
+    eh = -2 * (A'*dz + Etdz + dEtz);
 end
