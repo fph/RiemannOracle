8a28547010e5e7c187a47d7a26a9532f05160083
Federico Poloni <federico.poloni@unipi.it>
2024-02-14 21:10:25 +0100

    Added some final info to example_gcd

--- example_gcd.m
+++ example_gcd.m
@@ -9,8 +9,8 @@ d = 3;
 degp = length(p) - 1;
 degq = length(q) - 1;
 
-A = [1/degq * polytoep(p, degq-1).';
-     1/degp * polytoep(q, degp-1).'];
+A = [1/sqrt(degq) * polytoep(p, degq-1).';
+     1/sqrt(degp) * polytoep(q, degp-1).'];
 
 % Achtung: the following method to construct the basis works only if 
 % all coefficients in p and q are distinct
@@ -20,11 +20,22 @@ problem = nearest_nullity_structured_dense(P, A, d, true);
 [x cost info] = penalty_method(problem);
 
 Apert = A + info.Delta;
-pp = Apert(1, 1:length(p));
-qq = Apert(length(q), 1:length(q));
+pp = sqrt(degq) * Apert(1, 1:length(p));
+qq = sqrt(degp) * Apert(length(q), 1:length(q));
 
+fprintf('coefficients of p:\n')
+p
+fprintf('coefficients of q:\n')
+q
+fprintf('coefficients of pp:\n')
+pp
+fprintf('coefficients of qq:\n')
+qq
+fprintf('norm(p-pp)^2 + norm(q-qq)^2 = %e\n', norm(p-pp)^2 + norm(q-qq)^2);
+fprintf('svd of resultant(pp,qq):\n')
+svd(Apert).'
 fprintf('roots of pp:\n');
-r1 = roots(pp)
+r1 = roots(pp).'
 fprintf('roots of qq:\n');
-r2 = roots(qq)
+r2 = roots(qq).'
 fprintf('At least %d of them should coincide.\n', d);
