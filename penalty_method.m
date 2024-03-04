77ac4ec34c2cf7d686bada1ba8b0af54c23b2bd8
Federico Poloni <federico.poloni@unipi.it>
2024-03-04 12:28:34 +0100

    Added an adaptive decrease rule to penalty_method that replicates the code in nearest_matrix_polynomial.
    Now options.epsilon_decrease='f' should replicate the behavior of nearest_matrix_polynomial

--- penalty_method.m
+++ penalty_method.m
@@ -4,8 +4,17 @@ function [x cost info] = penalty_method(problem, x0, options)
 % x0 may be empty, as with other Manopt functions.
 %
 % Solve a structured distance problem with the penalty method.
+%
+%
 % If options.y is not empty, it is used as the starting value for the 
 % dual variable in an augmented Lagrangian method.
+% We recommend using options.y = 0 rather than the default [].
+%
+% options.epsilon_decrease is the strategy to use to reduce the
+% regularization paramter epsilon at each iteration. Supported values are:
+% a numerical constant (default=0.5), 'f' for an adaptive decrease based on 
+% the function value, 'g' for a decrease based on the value of the gradient.
+%
 
     default.starting_epsilon = 1;
     default.epsilon_decrease = 0.5;
@@ -51,22 +60,34 @@ function [x cost info] = penalty_method(problem, x0, options)
             y = y + 1/epsilon * cons;
         end
         
-        if isempty(options.epsilon_decrease)
-            % adaptive decrease
-            current_epsilon_decrease = 0.5;
-            eg = problem.genegrad(epsilon * current_epsilon_decrease, y, x, struct());
-            rg = problem.M.egrad2rgrad(x, eg);
-            while norm(rg) > 1e-4
-                current_epsilon_decrease = (1 + current_epsilon_decrease) / 2;
+        switch options.epsilon_decrease
+            case 'g' % adaptive decrease based on gradient value
+                current_epsilon_decrease = 0.5;
                 eg = problem.genegrad(epsilon * current_epsilon_decrease, y, x, struct());
                 rg = problem.M.egrad2rgrad(x, eg);
-                if current_epsilon_decrease > 0.95
-                    break
+                while norm(rg) > 1e-4
+                    current_epsilon_decrease = (1 + current_epsilon_decrease) / 2;
+                    eg = problem.genegrad(epsilon * current_epsilon_decrease, y, x, struct());
+                    rg = problem.M.egrad2rgrad(x, eg);
+                    if current_epsilon_decrease > 0.95
+                        break
+                    end
                 end
-            end
-            epsilon = epsilon * current_epsilon_decrease
-        else
-            epsilon = epsilon * options.epsilon_decrease;
+                epsilon = epsilon * current_epsilon_decrease;
+            case 'f' % adaptive decrease based on function value                
+                current_epsilon_decrease = 1e-2; % "aggressive" parameters based on the polynomial code and experiments
+                fval = problem.gencost(epsilon * current_epsilon_decrease, y, x, struct());
+                while fval > 2.5 * cost                     
+                    current_epsilon_decrease = current_epsilon_decrease * 1.1;
+                    fval = problem.gencost(epsilon * current_epsilon_decrease, y, x, struct());
+                    if current_epsilon_decrease > 0.95
+                        break
+                    end
+                end
+                epsilon = epsilon * current_epsilon_decrease;
+            otherwise  % fixed numerical value
+                assert(isscalar(options.epsilon_decrease));
+                epsilon = epsilon * options.epsilon_decrease;
         end    
     end
     Delta = regproblem.minimizer(x, struct());
