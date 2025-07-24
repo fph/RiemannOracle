c4b5422d35c24030e4ae59655dd4ef896daca680
Federico Poloni <federico.poloni@unipi.it>
2025-07-24 16:46:41 +0200

    Added an optional argument to autobasis

--- autobasis.m
+++ autobasis.m
@@ -1,7 +1,13 @@
-function P = autobasis(A)
+function P = autobasis(A, normalize)
 % Construct a perturbation basis that preserves the structure of A
 %
 % Equal values are kept equal, and zeros are kept zero.
+%
+% If normalized==true (default), normalize to norm(P(:,:,k),'fro')==1
+
+if not(exist('normalized', 'var')) || isempty(normalize)
+    normalize = true;
+end
 
 [m, n] = size(A);
 [I, J, V] = find(A);
@@ -14,6 +20,8 @@ P = zeros(m, n, p);
 for k = 1:length(V)
     P(I(k), J(k), IV(k)) = 1;
 end
-for k = 1:p
-    P(:,:,k) = P(:,:,k) / norm(P(:,:,k), 'fro');
+if normalize
+    for k = 1:p
+            P(:,:,k) = P(:,:,k) / norm(P(:,:,k), 'fro');
+    end
 end
